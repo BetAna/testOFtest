875c0224edfb3f557e625030899c96731baaec92
Bet Ana <anisoarabet@gmail.com>
2018-08-16 15:02:17 +0300

    dfsdf

--- TestingPurpose/UnitTests/UnitTests.cs
+++ TestingPurpose/UnitTests/UnitTests.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingPurpose;
 
 namespace UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class TestingPurposeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Sum_NegativeNumbers_CorrectCalculations()
         {
+            //Arrange
+            var toBeTested = new ToBeTestet();
+            var a = -10;
+            var b = -10;
+
+            //Act
+            var result = toBeTested.Sum(a, b);
+
+            //Assert
+            Assert.AreEqual(result, -20);
+        }
+        [TestMethod]
+        public void Sum_LargeNumbers_success()
+        {
+            //Arrange
+            var toBeTested = new ToBeTestet();
+            var a = 32000;
+            var b = 32000;
+
+            //Act
+            var result = toBeTested.Sum(a, b);
+
+            //Assert
+            Assert.AreEqual(result, 64000);
         }
     }
 }
