bd1621e34f98eeb58864363376d64ed7918687bf
Bet Ana <anisoarabet@gmail.com>
2018-08-16 12:55:46 +0300

    first commit

---
+++ TestingPurpose/TestingPurpose/ToBeTested.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPurpose
+{
+    public class ToBeTestet
+    {
+        public int Sum(int a, int b)
+        {
+            int sum = 0;
+            sum = a + b;
+            return sum;
+        }
+    }
+}
